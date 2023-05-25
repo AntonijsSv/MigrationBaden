8c069aca1314fa45016b0bb7a3c38942fc2e9517
AntSv <antonijs.svalovs@students.ksba.ch>
2023-05-25 21:31:53 +0200

    started work on map background, cleaned up the code slightly

--- mapTest.R
+++ mapTest.R
@@ -6,27 +6,34 @@ library(lintr) # code linting
 library(sf) # spatial data handling
 library(raster)
 library(viridis) # viridis color scale
-library(cowplot) # stack ggplots
 library(rmarkdown)
 library(readr)
 library(readxl)
 #canton_geo = read_sf("Boundary_Data/g2k23.shp")
-#lake_geo = read_sf("Boundary_Data/g2s23.shp")
+
 #country_geo= read_sf("Boundary_Data/g2l23.shp")
 
-municipality_geo = read_sf("Boundary_Data/g2g23.shp")
+municipality_geo <- read_sf("Boundary_Data/g2g23.shp")
 gemeinden_baden <- read_excel("2021_Gemeinden.xlsx") %>% 
-  mutate(GMDNR=`Gmd-Nr.`)
+  mutate(GMDNR=`Gmd-Nr.`,gemeinde_pop=`Gesamtbevölkerung`)
 
 gemeinden_coords <- left_join(municipality_geo,gemeinden_baden, by="GMDNR") %>%
   filter(!is.na(Gemeinde))
+  e_range <- c(min(gemeinden_coords$E_MIN)-1000,max(gemeinden_coords$E_MAX)+1000)
+  n_range <- c(min(gemeinden_coords$N_MIN)-1000,max(gemeinden_coords$N_MAX)+1000)
 
-pop_data <- read_delim("STATPOP2021_NOLOC.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
+  
+map <- raster("Maps/baden_relief1.tif")%>%
+  # hide relief outside of Switzerland by masking with country borders
+  mask(gemeinden_coords) %>%
+  as("SpatialPixelsDataFrame") %>%
+  as.data.frame() %>%
+  rename(value = `X02.relief.ascii`)
 
-ggplot(data=pop_data)+
+  
+ggplot(data=gemeinden_coords,aes(fill=Gesamtbevölkerung))+
   geom_sf(data = gemeinden_coords,
-          fill=Gesamtbevölkerung,
-          color = "navy",
+          color = "transparent",
           size = 0.5)+
   theme_minimal() +
   theme(
@@ -34,4 +41,14 @@ ggplot(data=pop_data)+
     axis.text.x = element_blank(),
     axis.text.y = element_blank(),
     axis.ticks = element_blank(),
-  )
+  ) +
+  scale_fill_viridis(
+    option = "magma",
+    alpha = 0.8,
+    begin = 0.1,
+    end = 0.9,
+    direction = -1
+  )+
+  geom_raster(data=map_df, aes(x=x, y=y, fill=baden_relief1))
+
+
