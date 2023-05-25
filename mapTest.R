b69f0d88e92f92061e0276a3fa4e0c4f7460423d
AntSv <antonijs.svalovs@students.ksba.ch>
2023-05-25 21:31:53 +0200

    started work on map background, cleaned up the code slightly

--- mapTest.R
+++ mapTest.R
@@ -6,32 +6,67 @@ library(lintr) # code linting
 library(sf) # spatial data handling
 library(raster)
 library(viridis) # viridis color scale
-library(cowplot) # stack ggplots
 library(rmarkdown)
 library(readr)
 library(readxl)
 #canton_geo = read_sf("Boundary_Data/g2k23.shp")
-#lake_geo = read_sf("Boundary_Data/g2s23.shp")
+
 #country_geo= read_sf("Boundary_Data/g2l23.shp")
 
-municipality_geo = read_sf("Boundary_Data/g2g23.shp")
+municipality_geo <- read_sf("Boundary_Data/g2g23.shp")
 gemeinden_baden <- read_excel("2021_Gemeinden.xlsx") %>% 
-  mutate(GMDNR=`Gmd-Nr.`)
+  mutate(GMDNR=`Gmd-Nr.`,gemeinde_pop=`Gesamtbevölkerung`)
 
 gemeinden_coords <- left_join(municipality_geo,gemeinden_baden, by="GMDNR") %>%
   filter(!is.na(Gemeinde))
+  e_range <- c(min(gemeinden_coords$E_MIN)-1000,max(gemeinden_coords$E_MAX)+1000)
+  n_range <- c(min(gemeinden_coords$N_MIN)-1000,max(gemeinden_coords$N_MAX)+1000)
 
-pop_data <- read_delim("STATPOP2021_NOLOC.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
+  
+map <- raster("Maps/baden_relief1.tif")%>%
+  # hide relief outside of Switzerland by masking with country borders
+  mask(gemeinden_coords) %>%
+  as("SpatialPixelsDataFrame") %>%
+  as.data.frame() %>%
+  rename(map_data = `baden_relief1`)
 
-ggplot(data=pop_data)+
-  geom_sf(data = gemeinden_coords,
-          fill=Gesamtbevölkerung,
-          color = "navy",
-          size = 0.5)+
+  
+ggplot(
+  data=gemeinden_coords,
+  aes(fill=Gesamtbevölkerung)
+  ) +
+  geom_raster(
+    data = map,
+    inherit.aes = FALSE,
+    aes(
+      x=x,
+      y=y,
+      alpha=map_data
+    )
+  ) +
+  scale_alpha(
+    name = "",
+    range = c(0.6,0),
+    guide = F
+  ) +
+  geom_sf(
+    data = gemeinden_coords,
+    color = "transparent",
+    size = 0.5)+
   theme_minimal() +
   theme(
     axis.line = element_blank(),
     axis.text.x = element_blank(),
     axis.text.y = element_blank(),
     axis.ticks = element_blank(),
+  ) +
+  scale_fill_viridis(
+    option = "magma",
+    alpha = 0.8,
+    begin = 0.1,
+    end = 0.9,
+    direction = -1
   )
+
+
+
