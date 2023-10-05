33be5ff7d0672f194e43a40bbf9af866cb8b87b1
AntSv <antonijs.svalovs@students.ksba.ch>
2023-10-05 11:43:07 +0200

    added years to columns

--- gws_baden.R
+++ gws_baden.R
@@ -11,6 +11,19 @@ communes_baden <- read_excel("analysis/2021_Gemeinden.xlsx") %>%
 
 years <- c(2021:2013)
 
+add_yr <- function(df,exemption,yr) {
+#'adds the suffix "_[yr]" to all columns but the exeption
+#'df is the dataframe
+#'exemption is a column you want unchanged -> column name, must be a string
+#'yr is the year or anything for that matter to discern it from other columns
+  for (column in 1:ncol(df)) {
+    if(colnames(df)[column]!=exemption){
+      colnames(df)[column] = paste0(colnames(df)[column],"_",yr)
+    }
+  }
+  return(df)
+}
+
 for (i in 1:length(years)) {
   print(years[i])
   if (years[i] >=2020) {
@@ -34,11 +47,13 @@ for (i in 1:length(years)) {
       filter(E_KOORD %in% (e_range[1]:e_range[2]), N_KOORD %in% (n_range[1]:n_range[2]))
     
     if (years[i] == 2021) {
-      gws_baden <- gws_yr#Adds 1st dataset to baden df
-      
+      gws_baden <- gws_yr%>%#Adds 1st dataset to baden df
+      add_yr("RELI",2021)
     }
     else {
-      dplyr::select(gws_yr, -c(X_KOORD,Y_KOORD,N_KOORD,E_KOORD)) #Removes Coordinates, as they stay constant throughout the years
+      gws_yr %>%
+      dplyr::select(-c(X_KOORD,Y_KOORD,N_KOORD,E_KOORD)) %>%#Removes Coordinates, as they stay constant throughout the years
+      add_yr("RELI",years[i])
       gws_baden <- full_join(gws_baden, gws_yr, by="RELI") 
       #Appends the new dataset to baden df, by joining by RELI, data belonging to each ha is put on the same row
     }
@@ -60,7 +75,8 @@ for (i in 1:length(years)) {
     gws_yr <- paste0("analysis/GWS/GWS",years[i],"_HA.csv")
     gws_yr <- read_delim(gws_yr, show_col_types = FALSE) %>%
       filter(E_KOORD %in% (e_range[1]:e_range[2]), N_KOORD %in% (n_range[1]:n_range[2]))%>%
-      dplyr::select(-c(X_KOORD,Y_KOORD,E_KOORD,N_KOORD))
+      dplyr::select(-c(X_KOORD,Y_KOORD,E_KOORD,N_KOORD))%>%#Removes Coordinates, as they stay constant throughout the years
+      add_yr("RELI",years[i])
     gws_baden <- full_join(gws_baden, gws_yr, by="RELI")
   }
   
