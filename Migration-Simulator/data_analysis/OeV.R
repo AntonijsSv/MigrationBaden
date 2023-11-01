a56f42e7cf88bb8f2a0d5a03194e04764f8decb3
jhy34205 <133877777+jhy34205@users.noreply.github.com>
2023-11-01 13:37:01 +0100

    I did a thing

--- Migration-Simulator/data_analysis/OeV.R
+++ Migration-Simulator/data_analysis/OeV.R
@@ -1,16 +1,16 @@
 library(readxl)
 library(dplyr)
 
-data1 <- read_excel("Migration-Simulator/main_data/services.xlsx")
+data1 <- read_excel("Migration-Simulator/main_data/OeV.xlsx")
 data2 <- read_excel("Migration-Simulator/main_data/population.xlsx")
 
 colnames(data1)
 colnames(data2)
 
 # Initialize lists and variables
-services <- list()
+job <- list()
 population <- list()
-services_percent <- list()
+job_percent <- list()
 population_percent <- list()
 percent_list <- c()  # Initialize as an empty numeric vector
 average_list <- c()  # Initialize as an empty numeric vector
@@ -25,23 +25,23 @@ while (x > 2011) {
     value2 <- data2[i, x]
     
     # Add values to vectors
-    services <- c(services, value1)
+    job <- c(job, value1)
     population <- c(population, value2)
     
     x <- int(x)
-    # Calculate services and population percent changes
+    # Calculate job and population percent changes
     if (i > 1) {
-      services_dif <- services[i] - services[i - 1]
+      job_dif <- job[i] - job[i - 1]
       population_dif <- population[i] - population[i - 1]
-      services_percent <- services_dif / services[i - 1]
+      job_percent <- job_dif / job[i - 1]
       population_percent <- population_dif / population[i - 1]
       
       # Append to percent lists
-      services_percent <- c(services_percent, services_percent)
+      job_percent <- c(job_percent, job_percent)
       population_percent <- c(population_percent, population_percent)
       
       # Calculate percent and append to the percent_list
-      percent <- population_percent[i] * services_percent[i] * 0.174242424
+      percent <- population_percent[i] * job_percent[i] * 0.174242424
       percent_list <- c(percent_list, percent)
     }
   }
